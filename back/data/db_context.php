627fd84733cbad24fec6a4776392938a8c5c755c
João Pedro <jpirmao37@gmail.com>
2024-06-04 15:02:58 -0300

    -add toda a base do backend

---
+++ back/data/db_context.php
@@ -0,0 +1,82 @@
+<?php 
+
+
+require_once("config.php");
+
+class DbContext{
+    private $host;
+    private $port;
+    private $dbname;
+    private $user;
+    private $pwd;
+
+    private $connection;
+
+public function __construct() {
+     $this->host  = MYSQL_DB_HOST;
+     $this->port = MYSQL_DB_PORT;
+     $this->dbname = MYSQL_DB_DATABASE;
+     $this->user = MYSQL_DB_USERNAME;
+     $this->pwd = MYSQL_DB_PASSWORD;
+}
+
+public function __connect() {
+    $this->connection = new mysqli($this->host, $this->user, $this->pwd, $this->dbname, $this->port);
+
+    if ($this->connection->connect_error) {
+        die("Falha de conexão: " . $this->connection->connect_error); 
+    }
+
+}
+
+public function close() {
+    $this->connection->close();
+}
+
+private function run_query_sql($query){
+    $result = $this->connection->query($query);
+
+    if (!$result) {
+            $error = array('error' => $this->connection->error);
+            return json_encode($error);
+    }
+
+    if ($result->num_rows > 0) {
+        $rows = array ();
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+
+        return json_encode($rows);
+    }
+
+    return json_encode($result);
+} 
+
+public function cadastrar($produto, $quantidade) {
+    $query = "INSERT INTO estoque_produtos (produto, quantidade) VALUES ('"
+    . $this->connection->real_escape_string($produto) . "', '"
+    . $this->connection->real_escape_string($quantidade) . "')" ;
+
+    return $this->run_query_sql($query);
+
+}
+
+public function consultar() {
+    $query = "SELECT * FROM estoque_produtos ORDER BY id";
+    return $this->run_query_sql($query);
+}
+
+public function editar($codigo, $produto, $quantidade) {
+    $query = "UPDATE estoque_produtos SET produto = '"
+    . $this->connection->real_escape_string($produto) . "', quantidade = " . "'"
+    . $this ->connection->real_escape_string($quantidade) . "'" . "WHERE id = " .  $codigo;
+
+    return $this->run_query_sql($query);
+}
+
+public function deletar($codigo){
+    $query = "DELETE FROM estoque_produtos WHERE id = " . $codigo;
+    return $this->run_query_sql($query);
+}
+} 
---
+++ back/index.php
@@ -0,0 +1,79 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+require("data/db_context.php");
+
+$tipo = 0;
+
+if (isset($_GET['tipo'])) {
+    $tipo = $_GET['tipo'];
+} else {
+    $error = array('error'=> 'Parametro TIPO não especificado!');
+    echo json_encode($error);
+}
+
+$db_context = new DbContext();
+$db_context->__connect(); // inicia a conexão com o backend
+
+// Avalia o resultado da espressao 'tipo', executando o respectivo codigo para cada caso
+switch ($tipo) {
+    // CREATE
+    case 1:
+        if (isset($_GET['produto']) && isset($_GET['quantidade'])) {
+            $produto = $_GET['produto'];
+            $quantidade = $_GET['quantidade'];
+
+            $result = $db_context->cadastrar($produto, $quantidade);
+            echo $result;
+        } else { 
+            $error = array('error' => "Parametro PRODUTO não especificado!");
+            echo json_encode($error);
+        }
+
+        break;
+    // READ    
+    case 2:
+        $result = $db_context->consultar();
+        echo $result;
+
+        break;
+    // UPDATE    
+    case 3:
+        if (isset($_GET["codigo"]) && isset($_GET['produto']) && isset($_GET['quantidade'])) {
+            $codigo = $_GET['codigo'];
+            $produto = $_GET['produto'];
+            $quantidade = $_GET['quantidade'];
+
+            $result = $db_context->editar($codigo, $produto, $quantidade);
+            echo $result;
+        } else {    
+            $error = array('error' => "Parametro CODIGO, PRODUTO ou QUANTIDADE não especificado!");
+            echo json_encode($error);
+        }
+
+        break;
+    // DELETE    
+    case 4:
+        if (isset($_GET['codigo'])) {
+            $codigo = $_GET['codigo'];
+
+            $result = $db_context->deletar($codigo);
+            echo $result;
+        } else {
+            $error = array('error'=> 'Parametro CODIGO nao especificado!');
+            echo json_encode($error);
+        }    
+
+        break;
+    default:
+        $error = array('error'=> 'Falha na requisicao');
+        echo json_encode($error);
+
+        break;
+}
+
+$db_context->close();
+
+
+/*The closing ?> tag MUST be omitted from files containing only PHP. Source: PSR-12*/
